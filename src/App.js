7f1377b6231874351fd9fb6a105b0cc2943d865e
Hyunsujk <hyunsujk@gmail.com>
2020-05-23 18:24:11 -0500

    Adds Tailwind CSS to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './tailwind.generated.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="head">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
