28ccb3b275cc4c74aa83378f418984e7cc65cee3
Hyunsujk <hyunsujk@gmail.com>
2020-05-27 16:27:16 -0500

    created card with image. added click function on the card.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './tailwind.generated.css';
+import React from 'react'
+import './App.css'
+import './tailwind.generated.css'
+import DisplayCard from './display-card/display-card'
 
 function App() {
   return (
     <div className="App" id="head">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello</h1>
+        <DisplayCard />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
