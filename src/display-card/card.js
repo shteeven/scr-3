3f5423c71de861c76278bdcb49665d7609bac858
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-06-06 10:51:29 -0500

    set image as background of div. test good

--- src/display-card/card.js
+++ src/display-card/card.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './card.scss'
 
 function Card(props) {
   const handleCardClick = (event) => {
     console.log('card clicked')
+  }
+
+  const imageDiv = {
+    backgroundImage: `url(${props.itemPic})`,
   }
 
   return (
@@ -10,7 +15,9 @@ function Card(props) {
       className="sm:w-2/3 md:w-1/3 lg:w-1/5 px-2 max-w-sm rounded overflow-hidden inline-block"
       onClick={handleCardClick}
     >
-      <img className="w-full" src={props.itemPic} alt="animal" />
+      <div className="imageWrapper">
+        <div className="imageContainer img" style={imageDiv}></div>
+      </div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{props.itemName}</div>
       </div>
