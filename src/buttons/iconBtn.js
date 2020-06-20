ceba511cebc2fdb6cd03cb0af0bbbdeb1da12ed2
hyunsujk <hyunsujk@gmail.com>
2020-06-20 12:52:24 -0500

    created files with initial code base for buttons

---
+++ src/buttons/iconBtn.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function FacebookBtn() {
+  return (
+    <div>
+      <p>Facebook</p>
+    </div>
+  )
+}
+
+function InstagramBtn() {
+  return (
+    <div>
+      <p>Instagram</p>
+    </div>
+  )
+}
+
+function TwitterBtn() {
+  return (
+    <div>
+      <p>Twitter</p>
+    </div>
+  )
+}
+
+function SoundcloudBtn() {
+  return (
+    <div>
+      <p>Soundcloud</p>
+    </div>
+  )
+}
+
+function YoutubeBtn() {
+  return (
+    <div>
+      <p>Youtube</p>
+    </div>
+  )
+}
+
+export default { FacebookBtn, InstagramBtn, TwitterBtn, SoundcloudBtn, YoutubeBtn }
---
+++ src/buttons/textBtn.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function DonateBtn() {
+  return (
+    <div>
+      <p>Donate</p>
+    </div>
+  )
+}
+
+function ListenBtn() {
+  return (
+    <div>
+      <p>Listen</p>
+    </div>
+  )
+}
+
+function LiveBtn() {
+  return (
+    <div>
+      <p>Live</p>
+    </div>
+  )
+}
+
+export default { DonateBtn, ListenBtn, LiveBtn }
