4b308bea0320980cb23a6ba4bc902bbff9a7a501
Hyunsujk <hyunsujk@gmail.com>
2020-06-25 00:39:18 -0500

    displayed live button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import './tailwind.generated.css'
 import DisplayCard from './display-card/display-card'
+import { LiveBtn } from './buttons/textBtn'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <h1>Hello</h1>
         <DisplayCard />
+        <LiveBtn />
       </div>
     </div>
   )
