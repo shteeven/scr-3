cee0167266e66f911c7aa4bf44502d8a327b693f
Hyunsujk <hyunsujk@gmail.com>
2020-07-21 22:44:06 -0500

    created base for buttons

--- src/buttons/textBtn.js
+++ src/buttons/textBtn.js
@@ -1,5 +1,53 @@
 import React from 'react'
 
+function HomeBtn() {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
+
+function LiveBtn() {
+  return (
+    <div>
+      <button class="bg-red-700 text-white font-bold py-2 px-4 rounded">Live</button>
+    </div>
+  )
+}
+
+function LatestNewsBtn() {
+  return (
+    <div>
+      <p>Latest News</p>
+    </div>
+  )
+}
+
+function ExploreBtn() {
+  return (
+    <div>
+      <p>Explore</p>
+    </div>
+  )
+}
+
+function ShopBtn() {
+  return (
+    <div>
+      <p>Shop</p>
+    </div>
+  )
+}
+
+function AboutBtn() {
+  return (
+    <div>
+      <p>About</p>
+    </div>
+  )
+}
+
 function DonateBtn() {
   return (
     <div>
@@ -16,12 +64,4 @@ function ListenBtn() {
   )
 }
 
-function LiveBtn() {
-  return (
-    <div>
-      <button class="bg-red-700 text-white font-bold py-2 px-4 rounded">Live</button>
-    </div>
-  )
-}
-
-export { DonateBtn, ListenBtn, LiveBtn }
+export { HomeBtn, LatestNewsBtn, ExploreBtn, ShopBtn, AboutBtn, DonateBtn, ListenBtn, LiveBtn }
